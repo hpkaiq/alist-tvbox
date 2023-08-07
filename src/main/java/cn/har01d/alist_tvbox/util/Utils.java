e03c0745d73d607e8343859d088f9131292bbce1
Harold <power0721@gmail.com>
2023-08-07 10:20:30 +0800

    新版AList

--- src/main/java/cn/har01d/alist_tvbox/util/Utils.java
+++ src/main/java/cn/har01d/alist_tvbox/util/Utils.java
@@ -21,7 +21,7 @@ public final class Utils {
     private static final int KB = 1024;
     private static final int MB = 1024 * KB;
     private static final int GB = 1024 * MB;
-    private static final Pattern EPISODE = Pattern.compile("^(.+)[sS]\\d{1,2}[eE]?$");
+    private static final Pattern EPISODE = Pattern.compile("^(.+)[sS]\\d{1,2}[eE]?\\d?$");
     private static final int[] mixinKeyEncTab = new int[]{
             46, 47, 18, 2, 53, 8, 23, 32, 15, 50, 10, 31, 58, 3, 45, 35, 27, 43, 5, 49,
             33, 9, 42, 19, 29, 28, 14, 39, 12, 38, 41, 13, 37, 48, 7, 16, 24, 55, 40,
