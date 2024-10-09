88a51aec3cbd7df901e88ee9c8090a8c4d286b9e
huangpingkang <huangpingkang@mail.eu321.com>
2024-10-09 10:07:00 +0800

    更新

--- scripts/downloadZx.sh
+++ scripts/downloadZx.sh
@@ -20,6 +20,18 @@ fi
 
 LOCAL_NUM=$(echo "$LOCAL" | tr -d '-')
 REMOTE_NUM=$(echo "$REMOTE" | tr -d '-')
+
+LOCAL_LEN=$(expr length "$LOCAL_NUM")
+REMOTE_LEN=$(expr length "$REMOTE_NUM")
+
+if [ "$LOCAL_LEN" -eq "8" ]; then
+    LOCAL_NUM="${LOCAL_NUM}0000"
+fi
+
+if [ "$REMOTE_LEN" -eq "8" ]; then
+    REMOTE_NUM="${REMOTE_NUM}0000"
+fi
+
 if (( REMOTE_NUM > LOCAL_NUM )); then
 
 echo "download ${REMOTE}" && \
