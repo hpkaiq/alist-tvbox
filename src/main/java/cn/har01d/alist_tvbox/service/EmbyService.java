5041d04c3e774838d980807fed9c5e09f07b6563
huangpingkang <hpkaiq@qq.com>
2025-08-17 10:16:42 +0800

    merge

--- src/main/java/cn/har01d/alist_tvbox/service/EmbyService.java
+++ src/main/java/cn/har01d/alist_tvbox/service/EmbyService.java
@@ -675,6 +675,7 @@ public class EmbyService {
             Call call = okHttpClient.newCall(request);
             Response response = call.execute();
             String html = response.body().string();
+            log.debug("postJson url:{} code:{} response:{}", url, response.code(), html);
             response.close();
             return html;
         } catch (Exception e) {
